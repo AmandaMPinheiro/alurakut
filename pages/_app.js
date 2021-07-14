7f7cf16113da35709b47e062f7ba83643f5c70d9
AmandaMPinheiro <amanda_mpinheiro@hotmail.com>
2021-07-13 22:33:12 -0300

    Aula 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,38 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/lib/AlurakutCommons';
 
 const GlobalStyle = createGlobalStyle`
+/* reset css */
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: sans-serif;
+    background-color: #D9E6F6;
   }
+
+  #__next{
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+/* reset imagem */
+  img{
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  ${AlurakutStyles}
 `
 
 const theme = {
